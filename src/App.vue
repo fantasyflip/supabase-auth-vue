38bfd48871382d2f4cd4b1fbe9c52bf77a7c712d
FantasyFlip <katzenberger.ph+sourcetree@gmail.com>
2021-12-04 16:27:45 +0100

    Added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <div id="app"></div>
+  <v-app>
+    <v-main> App </v-main>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
+
+  data() {
+    return {};
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
